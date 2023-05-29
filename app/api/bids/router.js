d12dd8ed286046680cb10223f2766928a3c7f4bb
StepanusRio <stepanusriodefaardian@gmail.com>
2023-05-29 11:19:40 +0700

    Added update route

--- app/api/bids/router.js
+++ app/api/bids/router.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { addBids, getBids, findBids } = require("./controller");
+const { addBids, getBids, findBids, updateBids} = require("./controller");
 const router = express.Router();
 
 router.post("/bids", addBids);
 router.get("/bids", getBids);
 router.get("/bids/:id", findBids);
+router.put("/bids/:id", updateBids);
 
 module.exports = router;
