04a1cd6b9eabf922d80defc513bb355d01277c2b
StepanusRio <stepanusriodefaardian@gmail.com>
2023-06-09 18:38:26 +0700

    Create Service SignUp that automate Create Ven/Gov

--- app/services/auth/signup.js
+++ app/services/auth/signup.js
@@ -3,27 +3,70 @@ const { createUserWithEmailAndPassword } = require("firebase/auth")
 const { auth } = require("../../config")
 
 const signUp = async (req) => {
+  const role = req.body.role;
   let token;
-  const newUsers = {
-    email: req.body.email,
-    password: req.body.password,
-    role: req.body.role
-  }
-  return createUserWithEmailAndPassword(auth, newUsers.email, newUsers.password).then((userCred) => {
-    const uid = userCred.user.uid;
-    token = userCred.user.getIdToken();
-    const userSchema = {
-      role: newUsers.role,
-      email: newUsers.email,
-      createdAt: new Date().toISOString(),
+  if (role === "Government") {
+    const newUsers = {
+      email: req.body.email,
+      password: req.body.password,
+      institute: req.body.institute,
+      address: req.body.address,
+      procurementId: "",
     }
-    return db.collection("User").doc(uid).set(userSchema);
-  }).then(() => {
-    return token
-  }).catch((error) => {
-    if (error.code === "auth/email-already-in-use")
-      return "USER ALREADY SIGNUP"
-  })
+    return createUserWithEmailAndPassword(auth, newUsers.email, newUsers.password).then((userCred) => {
+      const uid = userCred.user.uid
+      token = userCred.user.getIdToken()
+      const govSchema = {
+        email: newUsers.email,
+        institute: newUsers.institute,
+        address: newUsers.address,
+        procurementId: newUsers.procurementId,
+        createdAt: new Date().toISOString()
+      }
+      return db.collection(role).doc(uid).set(govSchema);
+    }).then(() => {
+      return token
+    }).catch((error) => {
+      if (error.code === "auth/email-already-in-use") {
+        return "USER ALREADY SIGNUP"
+      } else if (error.code === "auth/weak-password") {
+        return "PASSWORD TO WEAK"
+      }
+    })
+  } else if (role === "Vendor") {
+    const newUsers = {
+      email: req.body.email,
+      password: req.body.password,
+      institute: req.body.institute,
+      address: req.body.address,
+      npwp: req.body.npwp,
+      bidId: db.collection("Bids"),
+    }
+    return createUserWithEmailAndPassword(auth, newUsers.email, newUsers.password).then((userCred) => {
+      const uid = userCred.user.uid;
+      token = userCred.user.getIdToken();
+      const venSchema = {
+        email: req.body.email,
+        password: req.body.password,
+        institute: req.body.institute,
+        address: req.body.address,
+        npwp: req.body.npwp,
+        bidId: "",
+        createdAt: new Date().toISOString(),
+      }
+      return db.collection(role).doc(uid).set(venSchema)
+    }).then(() => {
+      return token
+    }).catch((error) => {
+      if (error.code === "auth/email-already-in-use") {
+        return "USER ALREADY SIGNUP"
+      } else if (error.code === "auth/weak-password") {
+        return "PASSWORD TO WEAK"
+      } else {
+        return error.code
+      }
+    })
+  }
 }
 
 module.exports = signUp;
