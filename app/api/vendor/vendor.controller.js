1b516e1ccb047ec0e01994d77dc23c301b633002
StepanusRio <stepanusriodefaardian@gmail.com>
2023-06-13 20:43:16 +0700

    Delete + Methode on both n fix Bug, add new field

--- app/api/vendor/vendor.controller.js
+++ app/api/vendor/vendor.controller.js
@@ -1,43 +1,27 @@
-const { addVendorService, 
-        getVendorServices, 
-        findVendorServices, 
-        updateVendorServices, 
-        deleteVendorServices 
-    } = require("../../services/api/vendor");
-
-exports.addVendor = async (req, res, next) => {
-  try {
-    const data = await addVendorService(req);
-    res.status(201).send({
-      message: "Succesful create a new Vendor",
-      data
-    })
-  } catch (err) {
-    res.status(400).send({ 
-        msg: "Filed adding data", 
-        err: err.message 
-    });
-    next(err)
-  }
-};
+const {
+  getVendorServices,
+  findVendorServices,
+  updateVendorServices,
+  deleteVendorServices,
+} = require("../../services/api/vendor");
 
 exports.getAllVendor = async (req, res, next) => {
   try {
     const data = await getVendorServices();
     if (data === "Not Found") {
       const error = res.status(200).send({
-        message: "Vendor Data Not Found"
-      })
-      return error
+        message: "Vendor Data Not Found",
+      });
+      return error;
     }
     res.status(200).send({
       message: "Success fetching data",
-      data
+      data,
     });
   } catch (err) {
-    res.status(400).send({ 
-        msg: "Failed to fetch data", 
-        err 
+    res.status(400).send({
+      msg: "Failed to fetch data",
+      err,
     });
     next(err);
   }
@@ -45,54 +29,54 @@ exports.getAllVendor = async (req, res, next) => {
 
 exports.findVendor = async (req, res, next) => {
   try {
-    const data = await findVendorServices(req)
+    const data = await findVendorServices(req);
     res.status(200).send({
-         msg: "Data Found", 
-         data 
-        });
-  } catch (err) {
-    res.status(400).send({ 
-        msg: "Failed to fetch data", 
-        err 
+      msg: "Data Found",
+      data,
     });
-    next(err)
+  } catch (err) {
+    res.status(400).send({
+      msg: "Failed to fetch data",
+      err,
+    });
+    next(err);
   }
 };
 
 exports.updateVendor = async (req, res, next) => {
   try {
     const data = await updateVendorServices(req);
-    res.status(200).send({ 
-        msg: "Data Updated", 
-        data 
+    res.status(200).send({
+      msg: "Data Updated",
+      data,
     });
   } catch (err) {
-    res.status(400).send({ 
-        msg: "Failed to Update", 
-        err: err.message 
+    res.status(400).send({
+      msg: "Failed to Update",
+      err: err.message,
     });
-    next(err)
+    next(err);
   }
 };
 
 exports.deleteVendor = async (req, res, next) => {
   try {
-    const data = await deleteVendorServices(req)
+    const data = await deleteVendorServices(req);
     if (data === "Not Found") {
       const error = res.status(404).send({
         message: "Delete Data Failed",
-        error: "ID NOT FOUND"
-      })
-      return error
+        error: "ID NOT FOUND",
+      });
+      return error;
     }
     res.status(200).send({
       msg: "Success Deleted Data",
-      data
+      data,
     });
   } catch (err) {
-    res.status(400).send({ 
-        msg: "Failed to Delete", 
-        err 
+    res.status(400).send({
+      msg: "Failed to Delete",
+      err,
     });
     next(err);
   }
--- app/api/vendor/vendor.router.js
+++ app/api/vendor/vendor.router.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const controller = require("./vendor.controller");
-const { authMiddlewares, isVendor } = require("../../middlewares/authentication");
-const upload = require("../../middlewares/multer")
+const {
+  authMiddlewares,
+  isVendor,
+} = require("../../middlewares/authentication");
+const upload = require("../../middlewares/multer");
 const router = express.Router();
 
-router.post("/vendor", isVendor, upload.fields([
-    { name: 'ProfilePicture', maxCount: 1 },
-    { name: 'filePortfolio', maxCount: 1 }
-    ]), controller.addVendor);
 router.get("/vendor", authMiddlewares, controller.getAllVendor);
 router.get("/vendor/:id", authMiddlewares, controller.findVendor);
-router.put("/vendor/:id", isVendor, controller.updateVendor);
-router.delete("/vendor/:id", isVendor, controller.deleteVendor);
-
+router.put(
+  "/vendor",
+  upload.fields([
+    { name: "profileImage", maxCount: 1 },
+    { name: "filePortfolio", maxCount: 1 },
+  ]),
+  isVendor,
+  controller.updateVendor
+);
+router.delete("/vendor", isVendor, controller.deleteVendor);
 module.exports = router;
