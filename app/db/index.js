365e4565dc35792d615176ffd9e8dc280ccc9dae
AndiraFaqih <afaqihm@gmail.com>
2023-06-14 23:17:22 +0700

    Update index.js

--- app/db/index.js
+++ app/db/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
+
 const admin = require("firebase-admin");
-const firebaseConfig = require("../../key.json") || JSON.parse(process.env.FIREBASE_KEY);
+const firebaseConfig = JSON.parse(process.env.FIREBASE_KEY || "{}");
 admin.initializeApp({
   credential: admin.credential.cert(firebaseConfig),
 });
