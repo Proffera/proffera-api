0400af68a56dcc56a056684c1703bb44ca4f8e46
AndiraFaqih <afaqihm@gmail.com>
2023-06-14 23:08:54 +0700

    fixing env

--- app/db/index.js
+++ app/db/index.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const firebaseConfig = require("../../key.json");
+const firebaseConfig = require("../../key.json") || JSON.parse(process.env.FIREBASE_KEY);
 admin.initializeApp({
   credential: admin.credential.cert(firebaseConfig),
 });
